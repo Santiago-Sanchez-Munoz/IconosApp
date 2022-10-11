837e68b3e713e24b35270264ed38dcf52cb723fc
Santiago-Sanchez-Munoz <62153661+Santiago-Sanchez-Munoz@users.noreply.github.com>
2022-10-10 23:21:58 -0500

    Taller10Iconos

---
+++ app/src/main/java/com/example/taller10/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.taller10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    String urlFb="https://www.facebook.com/";
+    String urlIn="https://www.instagram.com/";
+    String urlTw="https://twitter.com/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+    public void Instagram(View view){
+        Uri linkIn = Uri.parse(urlIn);
+        Intent Ii= new Intent(Intent.ACTION_VIEW, linkIn);
+        startActivity(Ii);
+    }
+
+    public void Facebook(View view){
+        Uri linkFb= Uri.parse(urlFb);
+        Intent iF= new Intent(Intent.ACTION_VIEW,linkFb);
+        startActivity(iF);
+    }
+
+    public void  Twitter(View view){
+        Uri linkTw = Uri.parse(urlTw);
+        Intent It= new Intent(Intent.ACTION_VIEW, linkTw);
+        startActivity(It);
+    }
+}
